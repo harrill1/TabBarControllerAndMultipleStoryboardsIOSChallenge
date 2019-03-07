4b2b157e0133afdfcfc7151e42463d114032a137
harrill1 <hharrill12@gmail.com>
2019-03-06 20:02:45 -0600

    Added Complete Tab Bar Application

---
+++ Tab Bar Controller/Tab Bar Controller/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Tab Bar Controller
+//
+//  Created by Hayden Harrill on 3/6/19.
+//  Copyright © 2019 Hayden Harrill. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Tab Bar Controller/Tab Bar Controller/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Tab Bar Controller
+//
+//  Created by Hayden Harrill on 3/6/19.
+//  Copyright © 2019 Hayden Harrill. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
